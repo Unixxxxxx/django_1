34d3b1c78d272c57d7c48a6e2a25e778a6817cfb
Unixxxxxx <sudhanshuroyss208@gmail.com>
2024-09-20 21:05:08 +0530

    add emailfield

--- appdb/models.py
+++ appdb/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Db(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
+    email = models.EmailField(null ='True')
+
 
     def __str__(self):
         return self.name 
